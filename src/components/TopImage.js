5e8e137d524a53a5a5949bab7334018db9957e73
ndk2810 <khangdeptrai2810@gmail.com>
2021-03-26 20:41:26 +0700

    Initial commit

---
+++ src/components/TopImage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TopImage = () => {
+    return (
+        <div className="top-image">
+            <div className="top-image-text">
+                <h1>Tour</h1>
+                <p>Khám phá những điểm đến mới và độc đáo</p>
+            </div>
+        </div>
+    )
+}
+
+export default TopImage
