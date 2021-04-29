228c1813889dbe2c48e043ccd355a6593b6be173
ndk2810 <khangdeptrai2810@gmail.com>
2021-04-29 10:42:51 +0700

    Them hinh anh

--- backend/routes/diaDiem.js
+++ backend/routes/diaDiem.js
@@ -9,7 +9,42 @@ router.route('/').get((req, res) => {
             if (err)
                 console.log(err)
             else {
-                console.log(result)
+                res.send(result.recordset)
+            }
+        })
+    })
+})
+
+router.route('/add').post((req, res) => {
+    db.connect().then(() => {
+        const TenDiaDiem = req.body.tenDiaDiem
+        const HinhAnhDiaDiem = req.body.hinhAnh
+        //simple query
+        const queryString = `DECLARE @str VARCHAR(MAX);
+        SET @str = '${HinhAnhDiaDiem}';
+        INSERT INTO DIADIEM(TenDiaDiem, HinhAnhDiaDiem)
+        VALUES (N'${TenDiaDiem}', cast(N'' as xml).value('xs:base64Binary(sql:variable("@str"))', 'VARBINARY(MAX)'))`;
+
+        db.request().query(queryString, (err, result) => {
+            if (err)
+                console.log(err)
+            else {
+                res.send(result.recordset)
+            }
+        })
+    })
+})
+
+router.route('/remove').post((req, res) => {
+    db.connect().then(() => {
+        const IDDiaDiem = req.body.IDDiaDiem
+        //simple query
+        const queryString = `DELETE FROM DIADIEM WHERE IDDiaDiem = ` + IDDiaDiem;
+
+        db.request().query(queryString, (err, result) => {
+            if (err)
+                console.log(err)
+            else {
                 res.send(result.recordset)
             }
         })
--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,9 @@ const sql = require('mssql/msnodesqlv8')
 const cors = require('cors')
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
+
 
 //IMPORT ROUTERS
 const diaDiemRouter = require('./routes/diaDiem')
