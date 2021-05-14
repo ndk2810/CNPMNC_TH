3fe2ec15d327fdd54c67f6967c265ace9b8e9aec
vietmit37 <nthanhtung1440@gmail.com>
2021-05-14 22:00:07 +0700

    add

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './rep  ortWebVitals';
+import reportWebVitals from './reportWebVitals';
 
 //IMPORT CSS HERE
 import './styles/index.css'
