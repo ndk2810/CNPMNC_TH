13e887424989b4c3c7239f9cfc6c30dbd8704278
ndk2810 <khangdeptrai2810@gmail.com>
2021-05-14 21:00:42 +0700

    tim kiem xong

--- src/components/PageDiaDiem/SearchBoxCT.js
+++ src/components/PageDiaDiem/SearchBoxCT.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import { useHistory } from "react-router-dom";
 
 const SearchBox = () => {
+    const history = useHistory()
+
+    const timKiem = () => {
+        const tuKhoaTimKiem = document.getElementById('tuKhoaTimKiem').value
+        history.push("/timKiem?TenDiaDiem=" + tuKhoaTimKiem)
+        window.location.reload()
+    }
     return (
         <div className="search search-chiTiet">
             <img src="https://d1785e74lyxkqq.cloudfront.net/webxpe-desktop/_next/static/841107e3ee5dae66f6c15c41a74f5cce.svg" alt="search-logo" className="search-logo" />
-            <input type="text" placeholder="Tìm hoạt động hoặc điểm đến" />
-            <input type="submit" value=" " />
+            <input type="text" placeholder="Tìm hoạt động hoặc điểm đến" id="tuKhoaTimKiem" />
+            <input type="submit" value=" " onClick={timKiem} />
         </div>
     )
 }
--- src/components/PageIndex/SearchBox.js
+++ src/components/PageIndex/SearchBox.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import { useHistory } from "react-router-dom";
 
 const SearchBoxCT = () => {
+    const history = useHistory()
+
+    const timKiem = () => {
+        const tuKhoaTimKiem = document.getElementById('tuKhoaTimKiem').value
+        history.push("/timKiem?TenDiaDiem=" + tuKhoaTimKiem)
+    }
+
     return (
         <div className="search">
             <img src="https://d1785e74lyxkqq.cloudfront.net/webxpe-desktop/_next/static/841107e3ee5dae66f6c15c41a74f5cce.svg" alt="search-logo" className="search-logo" />
-            <input type="text" placeholder="Tìm hoạt động hoặc điểm đến" />
-            <input type="submit" value=" " />
+            <input type="text" placeholder="Tìm hoạt động hoặc điểm đến" id="tuKhoaTimKiem" />
+            <input type="submit" value=" " onClick={timKiem} />
         </div>
     )
 }
