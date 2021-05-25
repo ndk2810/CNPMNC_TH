bc60bc817993381a24619bc5249db1c677262d15
ndk2810 <khangdeptrai2810@gmail.com>
2021-05-25 21:10:14 +0700

    theloai

--- src/components/PageIndex/TourTrangChu.js
+++ src/components/PageIndex/TourTrangChu.js
@@ -2,8 +2,6 @@ import { Link } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 
 const TourTrangChu = ({ Tour }) => {
-    //Link dùng để chuyển trang
-    const linkTo = '/thongTinTour?IDTour=' + Tour.IDTour
 
     //GET Hình ảnh bìa của tour
     let [HinhAnh, setHinhAnh] = useState([])
@@ -20,6 +18,24 @@ const TourTrangChu = ({ Tour }) => {
                 console.log(err);
             });
     }, []);
+
+    //GET thể loại tour
+    let [TheLoaiTour, setTheLoaiTour] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:5000/theLoaiTour/" + Tour.IDTheLoaiTour)
+            .then((data) => {
+                return data.json();
+            })
+            .then((data) => {
+                if (data.length > 0) setTheLoaiTour(data[0]);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
+
+    //Link dùng để chuyển trang
+    const linkTo = '/thongTinTour?IDTour=' + Tour.IDTour + "&TenTheLoai=" + TheLoaiTour.TenTheLoaiTour
 
     let src = 'url(https://semantic-ui.com/images/wireframe/white-image.png)'
     if (HinhAnh.HinhAnh) {
