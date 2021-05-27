55181156a3ea6905b4a24e09c3045d21334d6f30
vietmit37 <nthanhtung1440@gmail.com>
2021-05-28 01:15:18 +0700

    add

--- src/components/PageChiTietTour/DatTour.js
+++ src/components/PageChiTietTour/DatTour.js
@@ -8,17 +8,17 @@ const DatTour = ({ Tour, KhungThoiGian }) => {
 		<div className="Dattour">
 			<div className="Content-Dattour">Khả dụng vào ngày khác</div>
 			<div className="Chon-Dattour">
-				<h3 className="Content-Chon-Dattour">
-					{Tour.TenTour}
-				</h3>
+				<h3 className="Content-Chon-Dattour">{Tour.TenTour}</h3>
 				<div className="Btn-Chon-giatien">
-					<div className="giatien-chitiet-tour giatien-chitiet">{Tour.GiaNguoiLon} VNĐ</div>
+					<div className="giatien-chitiet-tour giatien-chitiet">
+						{Tour.GiaNguoiLon} VNĐ
+					</div>
 					<div className="btn-group" role="group" aria-label="Basic example">
 						{professions.map((profession) => (
 							<button
 								type="button"
 								key={profession}
-								className={"btn-Chon"}
+								className={"btn-Chon btn-timtour"}
 								onClick={() => setMyProfession(profession)}
 							>
 								{profession.toLocaleUpperCase()}
@@ -26,7 +26,9 @@ const DatTour = ({ Tour, KhungThoiGian }) => {
 						))}
 					</div>
 					<div className="show-NgayDat">
-						{myProfession === "Chọn" && <NgayDatTour Tour={Tour} KhungThoiGian={KhungThoiGian} />}
+						{myProfession === "Chọn" && (
+							<NgayDatTour Tour={Tour} KhungThoiGian={KhungThoiGian} />
+						)}
 					</div>
 				</div>
 			</div>
